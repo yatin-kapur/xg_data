9c9d1f191420e5b90b5fd97ba92f064194bcc393
yatin-kapur <yatin.aol@gmail.com>
2018-12-20 11:58:55 -0500

    heights using sql

--- get_height.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-
-db_dict = dbconfig.read_db_config()
-db = MySQLdb.connect(host=db_dict['host'],
-                         user=db_dict['user'],
-                         passwd=db_dict['password'],
-                         db=db_dict['database'])
-cursor = db.cursor()
-
